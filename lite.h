777502f1757da092a758d483e874f59e5ba66ab1
Joachim Nilsson <troglobit@gmail.com>
2012-10-02 06:10:09 +0200

    Fix I/O plugin watcher, load plugins earlier to enable a new early hook!
    Restructure start by calling the plugin loader earlier. This makes it
    possible to run hooks even earlier.  This also adds a new hook:
    HOOK_ROOTFS_UP which runs as soon as the system root (/) is available.
    
    Another very notable change is the slow refactor of helpers.c into a finit
    helper library called libite.so (-lite). This is a light-weight
    
    Also, lots of misc. fixes all over the place. Signal handling still really
    needs to be looked into once more!
    
    Makefile:
            Add dependency handling for parallell build
            Add libite library
    
    ChangeLog:
            Update with new 1.4 release. There is a lot of fixes!
    
    libite/:
            Collection of utility functions and C library extensions for
            finit & its plugins
    
    reboot.c:
            New reboot tool. Simply sends SIGINT to PID 1
    
    finit.h:
            Cleanup old LISTEN_INITCTL define, code is in an optional plugin now
    
    finit.c:
            Let mdev/udev run *after* cls()
            Add useful debug statement for /proc/cmdline
            Earlier plugin load => new early hook point
            Show OK/FAIL for total plugin load, not for each plugin => speedup
    
    helpers.h:
            Rename syslog macros to prevent common name clashes (DEBUG/ERROR)
            Add __FILE__ to _e(), _d() and _pe() macros
    
    helpers.c:
            In ifconfig() the addr and mask arguments are now optional
            The copyfile() has been moved to libite.so
            Temporarily remove const directive from some functions to build
            Use DFLSIG() macro in run_getty() to simplify code
    
    plugin.h:
            Add new HOOK_ROOTFS_UP hook
    
    plugin.c:
            Fix init_plugins() to actually register the plugin's I/O fd
            Skip verbose listing of all plugins in plugin_load_all()
    
    svc.c:
            svc_start(): Dito
    
    plugins/Makefile:
            Inherit CFLAGS and LDFLAGS, but make sure to filter out -rdynamic
            from LDFLAGS, it's used by the daemon, only
    
    plugins/initctl.c:
            New includes after helpers.h refactor
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ lite.h
@@ -0,0 +1,61 @@
+/* Collection of utility funcs and C library extensions for finit & its plugins
+ *
+ * Copyright (c) 2008-2010  Claudio Matsuoka <cmatsuoka@gmail.com>
+ * Copyright (c) 2008-2012  Joachim Nilsson <troglobit@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef FINIT_LITE_H_
+#define FINIT_LITE_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int     fexist     (char *file);
+int     fisdir     (char *file);
+mode_t  fmode      (char *file);
+
+ssize_t copyfile   (char *src, char *dst, int len);
+int     movefile   (char *src, char *dst);
+int     copy_filep (FILE *src, FILE *dst);
+
+int     dir        (const char *dir, const char *type, int (*filter) (const char *file), char ***list, int strip);
+int     rsync      (char *src, char *dst, int delete, int (*filter) (const char *file));
+
+static inline int fisslashdir(char *dir)
+{
+   if (!dir)             return 0;
+   if (strlen (dir) > 0) return dir[strlen (dir) - 1] == '/';
+                         return 0;
+}
+
+#endif /* FINIT_LITE_H_ */
+
+/**
+ * Local Variables:
+ *  version-control: t
+ *  indent-tabs-mode: t
+ *  c-file-style: "linux"
+ * End:
+ */
+
