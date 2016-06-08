e8a3117158eaad4a665782fd2f9e48b7553451fc
Joachim Nilsson <troglobit@gmail.com>
2016-06-08 06:23:34 +0200

    fparseln: Portability fix _DIAGASSERT()
    Only define _DIAGASSERT() if it's not already defined.
    For building libite on NetBSD.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- fparseln.c
+++ fparseln.c
@@ -36,7 +36,9 @@ __RCSID("$NetBSD: fparseln.c,v 1.10 2009/10/21 01:07:45 snj Exp $");
 #include <stdlib.h>
 #include "lite.h"
 
+#ifndef _DIAGASSERT
 #define _DIAGASSERT(t)
+#endif
 
 static int isescaped(const char *, const char *, int);
 
