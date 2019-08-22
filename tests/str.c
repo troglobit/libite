13ffe87d1053a77b262e98640bc7d32e72a22242
Joachim Nilsson <troglobit@gmail.com>
2019-08-22 22:26:07 +0200

    tests: Update unit test for string functions
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/str.c
+++ tests/str.c
@@ -2,16 +2,24 @@
 
 int main(void)
 {
+	char buf[10];
 	int val;
 
 	val = atonum("42");
 	if (val != 42)
 		return 1;
-	
+
 	if (!string_match("eth1", "eth1234"))
 		return 1;
 
 	if (string_compare("eth1", "eth1234"))
+		return 1;
+
+	if (!string_case_compare("somestring", "SoMeSTRING"))
+		return 1;
+
+	strlcpy(buf, "tooloongstring", sizeof(buf));
+	if (buf[9] == 'r')
 		return 1;
 
 	return 0;
