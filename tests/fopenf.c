f85ff3c835db1905127de05636ca427b66a7f5f3
Joachim Nilsson <troglobit@gmail.com>
2020-04-27 23:18:47 +0200

    tests: Add fremove() pass to fopenf() unit test for cleanup
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/fopenf.c
+++ tests/fopenf.c
@@ -6,6 +6,7 @@ int main(void)
 	char *dir = "/tmp";
 	char *file = "__foo__";
 
+	printf("Pass 1/2: fopenf() ===========================\n");
 	fp = fopenf("w", "%s/%s", dir, file);
 	if (fp) {
 		fputs("foo", fp);
@@ -13,6 +14,12 @@ int main(void)
 	}
 
 	fail_unless(fexist("/tmp/__foo__"));
+
+	printf("Pass 2/2: fremove() ==========================\n");
+	fremove("%s/%s", dir, file);
+	fail_unless(!fexist("/tmp/__foo__"));
+
+	printf("DONE =========================================\n");
 
 	return 0;
 }
